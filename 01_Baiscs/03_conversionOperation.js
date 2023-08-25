80453ac8fa50ad6c7d2c6e817d749d9c909fecb0
Ayush Dubey <96471916+ayushcoder2021@users.noreply.github.com>
2023-08-25 16:55:42 +0000

    conversion

---
+++ 01_Baiscs/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "ayush"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "24" => 24
+// "24a" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "ayush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false
+// "" => false
+// "ayush" => true
+
+let someNumber = 66
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
