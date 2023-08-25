96c8604d2a3cceb177cbf9802311b32678149859
Ayush Dubey <96471916+ayushcoder2021@users.noreply.github.com>
2023-08-25 16:24:25 +0000

    datatypes

---
+++ 01_Baiscs/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(6 + 6) // we are using nodejs, not browser
+
+console.log(3 
+    + 4) // code readiblity should be high
+    
+    console.log("I am a developer")
+
+    let name = "Ayush"
+    let age = 21
+    let isLoggedIn = false
+    let state;
+    
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+    
+    
+    // object
+    
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
