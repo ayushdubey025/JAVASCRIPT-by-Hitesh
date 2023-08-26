ea557bd42d01128643e1b0033fbce4c432366a7a
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-08-26 16:54:53 +0000

    comparision

---
+++ 01_Baiscs/04_Comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // This type of convesions is always confusing.
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === it checks values stricktly
+
+console.log("2" === 2);
