3d55de1ed6ffd746b183de956c8e555b91d4e72e
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-08-28 16:18:53 +0000

    stack and heap

--- 01_Baiscs/Datatypes-summary.js
+++ 01_Baiscs/Datatypes-summary.js
@@ -44,3 +44,29 @@ const myFunction = function(){
 //console.log(typeof anotherId); // return type - symbol
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack (Primitive),    Heap (Non Primitive)
+
+let myname1 = "ayush";
+
+let myname2 = myname1;
+
+myname2 = "dubey";
+
+console.log(myname1);
+console.log(myname2);
+
+let user1 ={
+    email: "ayush@gmail.com",
+    upi: "1234567890"
+}
+
+let user2 = user1;
+
+user2.upi = "9876543210";
+
+console.log(user1.upi);
+console.log(user2.upi);
