a35b68405e025c8fc8a67268b3d0a4b1778ab897
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-08-30 16:51:29 +0000

    Date in JS

---
+++ 01_Baiscs/07_DatesinJS.js
@@ -0,0 +1,53 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// console.log(myDate.valueOf());
+// console.log(myDate.getTime());
+// console.log(myDate.getTimezoneOffset());
+
+
+// *** IMPORTANT NOTE: DATE is a "Object", not a primitive
+
+//console.log(typeof myDate); // => "object"
+
+// let newDate = new Date(2023,5,8,4,5);
+// console.log(newDate.toString());
+
+// let newDate = new Date("2023-1-18");
+//let newDate = new Date("08-30-2023");   // in india we prefer mm-dd-yyyy system 
+// console.log(newDate.toLocaleString());
+
+
+
+//Time stamp is use in creating polls for timely updates
+
+// let myTimeStamp = new Date().now();
+let myTimeStamp = Date.now (); // we can also use "Date.now()"
+
+console.log(myTimeStamp);   // We get data in miliseconds
+//console.log(newDate.getTime());    // We get data in miliseconds
+
+
+
+
+//*** IMPORTANT NOTE: For Interviews we need to get data in Seconds
+console.log(Math.floor(myTimeStamp/1000));   // We get data in Seconds
+
+let newDate2 = new Date();
+console.log(newDate2.getDay());
+
+
+// `${newDate.getDay()} and the time `
+
+newDate3.toLocaleString('default', {
+    weekday: "long",
+    
+})
