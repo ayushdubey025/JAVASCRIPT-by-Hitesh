ce28f6be0e78754b24506c879b0b52398441692a
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-09-09 16:24:13 +0000

    IIFE

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,33 @@
+//  Immediately Invoked Function Expressions (IIFE)
+
+// We use IIFE for clear the polution which is created by Global Scope
+
+// function chai() {
+//   console.log(`DB CONNECTED`);
+// }
+// ();  // Syntax error
+
+
+(function chai() {
+    // Named IIFE
+    console.log(`DB CONNECTED`);
+  })
+  ();    // In IIFE we need to write our function in Paranthesis and 
+  //    at last end with ;
+
+
+  // IIFE uses in ARROW Functions
+
+//   const AnyValue = (num) => {
+//     console.log(` You age is ${num}`)
+//     return num;
+//   }
+//   AnyValue(19)
+
+
+( (num) => {
+    // Unnamed IIFE
+    console.log(`You age is ${num}`)
+    return num;
+  })
+  (24)    // We don't have to write const for arrow function in IIFE
