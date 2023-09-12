249c60aa96c39fcb8c33ad865a098f914345ee48
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-09-12 16:14:33 +0000

    Switch

---
+++ 04_Control_Flow/02_Switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
