25270f1bae28e701b15b24a4c71f2deca5e2ef7c
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-09-17 15:32:18 +0000

    six

---
+++ 05_Iterations/06_Six.js
@@ -0,0 +1,9 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
