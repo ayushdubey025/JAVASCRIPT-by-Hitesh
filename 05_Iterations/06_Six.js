0d7638ed1a9a5a523faff55f3af85a3d6aded9ce
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-09-19 16:02:35 +0000

    six

--- 05_Iterations/06_Six.js
+++ 05_Iterations/06_Six.js
@@ -8,3 +8,18 @@
 
 // console.log(values); 
 
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
