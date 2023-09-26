42b436787dbffb48dd54a00333f250a3e7412c19
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2023-09-26 17:22:45 +0000

    seven and eight

--- 05_Iterations/07_Seven.js
+++ 05_Iterations/07_Seven.js
@@ -1,7 +1,12 @@
 const myNumber = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const newNumber = myNumber.map((num) => {
-  return num + 10;
-});
+// const newNumber = myNumber.map((num) => {
+//   return num + 10;
+// });
+
+const newNumber = myNumber
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
 console.log(newNumber); 
- 
