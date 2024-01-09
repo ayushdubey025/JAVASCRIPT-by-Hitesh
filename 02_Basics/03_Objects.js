02c021b5c0384bc64bd08bc8c76fbbe24426ae6e
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2024-01-09 16:38:09 +0000

    objects

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -7,11 +7,11 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Ayush",
+    "full name": "Ayush Dubey",
     [mySym]: "mykey1",
     age: 18,
-    location: "Jaipur",
+    location: "Dharwad",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
@@ -23,19 +23,20 @@ const JsUser = {
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+Object.freeze(JsUser)
+JsUser["email"] = "hitesh@microsoft.com"
+console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
+
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
 
 
 
