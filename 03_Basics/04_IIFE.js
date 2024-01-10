248ba50abb1d18546e8b5985f08165ddfaa8ef59
Ayush Dubey <96471916+dubey024@users.noreply.github.com>
2024-01-10 09:18:13 +0000

    IIFE for function and arrow

--- 03_Basics/04_IIFE.js
+++ 03_Basics/04_IIFE.js
@@ -13,21 +13,21 @@
     console.log(`DB CONNECTED`);
   })
   ();    // In IIFE we need to write our function in Paranthesis and 
-  //    at last end with ;
+  //    at last end with ; for execute another function otherwise we will get error
 
 
   // IIFE uses in ARROW Functions
 
-//   const AnyValue = (num) => {
-//     console.log(` You age is ${num}`)
-//     return num;
-//   }
-//   AnyValue(19)
+  const AnyValue = (num) => {
+    console.log(` Your age is ${num}`)
+    return num;
+  }
+  AnyValue(19);
 
 
 ( (num) => {
     // Unnamed IIFE
-    console.log(`You age is ${num}`)
+    console.log(`Your age is ${num}`)
     return num;
   })
   (24)     // We don't have to write const for arrow function in IIFE
