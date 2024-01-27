533545be5b7316b42f879ab29a509d7f3e392f7f
Ayush Dubey <96471916+iamdubey024@users.noreply.github.com>
2024-01-27 07:46:27 +0000

    Project - Unlimited Color Changer

---
+++ 07_Projects/6-UnlimitedColors/unlimitedColors.js
@@ -0,0 +1,32 @@
+const startcolor = document.querySelector('.start');
+const stopcolor = document.querySelector('.stop');
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startChangingColor = function ()
+{
+  if(!intervalId)
+  {
+    intervalId = setInterval(changeBGcolor, 1000);
+  }
+ function changeBGcolor () {
+    document.body.style.backgroundColor = randomColor();
+  };
+ 
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+startcolor.addEventListener('click', startChangingColor, false);
+
+stopcolor.addEventListener('click', stopChangingColor, false);
