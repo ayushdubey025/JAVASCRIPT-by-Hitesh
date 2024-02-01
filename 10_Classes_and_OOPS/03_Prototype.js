beb8e2b0bfbc7cb83faaa209e48b5063ea0880b5
Ayush Dubey <96471916+iamdubey024@users.noreply.github.com>
2024-02-01 11:04:55 +0000

    Prototype

--- 10_Classes_and_OOPS/03_Prototype.js
+++ 10_Classes_and_OOPS/03_Prototype.js
@@ -38,3 +38,39 @@ Array.prototype.heyAyush = function(){
 
 // heroPower.heyAyush();  
 
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"Ayush".trueLength()
+"Internship".trueLength()
